80abf78908f2fc1abb5c0cde8d62214d3238d905
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-05-07 18:21:19 +0300

    first commit

---
+++ FaceBookPage/FaceBookPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceBookPage
+//
+//  Created by Владимир on 07.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
