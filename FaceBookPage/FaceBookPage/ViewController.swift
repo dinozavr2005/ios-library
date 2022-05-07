86b6a8043116636cd6130da744bfc39f9e9a44d2
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-05-07 18:47:55 +0300

    add readme file in noStoryBoard

--- FaceBookPage/FaceBookPage/ViewController.swift
+++ FaceBookPage/FaceBookPage/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let stackView = UIStackView()
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
-
-
 }
+
+extension ViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(label)
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
 
