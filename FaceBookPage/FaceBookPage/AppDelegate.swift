03da5de7c100c43c126d8edf95e8a4686c369066
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-05-08 12:04:31 +0300

    release app

--- FaceBookPage/FaceBookPage/AppDelegate.swift
+++ FaceBookPage/FaceBookPage/AppDelegate.swift
@@ -7,19 +7,29 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
-        window?.rootViewController = ViewController()
+        
+        let feedController = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
+        let navigatorController = UINavigationController(rootViewController: feedController)
+        window?.rootViewController = navigatorController
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 51/255, green: 90/255, blue: 149/255, alpha: 1)
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        // even after setting this the status bar is still black...
+        application.statusBarStyle = .lightContent
+        // fix is to add the following Info.plist entry
+        // `View controller-based status bar` boolean value of `NO`
         
         return true
     }
-}
 
+}
